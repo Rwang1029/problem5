76588433eb867a4560ac881919e1afd9243dd743
Rwang1029 <71751305+Rwang1029@users.noreply.github.com>
2020-11-18 15:08:33 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onPinPressed(TouchPin.P0, function () {
+    o = randint(0, 10)
+    basic.showNumber(o)
+    if (o <= 3) {
+        basic.showIcon(IconNames.Sad)
+        basic.showString("It's alright!")
+    } else if (o <= 7) {
+        basic.showIcon(IconNames.Happy)
+        basic.showString("Good day!")
+    } else if (o <= 10) {
+        basic.showIcon(IconNames.Surprised)
+        basic.showString("Lucky day!")
+    } else {
+    	
+    }
+})
+let o = 0
+basic.showString("Today's Fortune")
+basic.forever(function () {
+	
+})
